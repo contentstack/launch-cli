7c3a0c8db7e1f01873400145cbaf0c04a99f2aa6
Chhavi-Mandowara <chhavi.mandowara@contentstack.com>
2025-04-30 15:41:43 +0530

    fix: allow --type flag to create new project without prompting to select  provider

--- src/commands/launch/index.ts
+++ src/commands/launch/index.ts
@@ -21,9 +21,13 @@ export default class Launch extends BaseCommand<typeof Launch> {
     '<%= config.bin %> <%= command.id %> --data-dir <path/of/current/working/dir> --redeploy-last-upload',
     '<%= config.bin %> <%= command.id %> --config <path/to/launch/config/file> --type <options: GitHub|FileUpload>',
     '<%= config.bin %> <%= command.id %> --environment=<value> --redeploy-latest',
+    // eslint-disable-next-line max-len
     '<%= config.bin %> <%= command.id %> --config <path/to/launch/config/file> --type <options: GitHub|FileUpload> --name=<value> --environment=<value> --branch=<value> --build-command=<value> --framework=<option> --org=<value> --out-dir=<value>',
+    // eslint-disable-next-line max-len
     '<%= config.bin %> <%= command.id %> --config <path/to/launch/config/file> --type <options: GitHub|FileUpload> --name=<value> --environment=<value> --branch=<value> --build-command=<value> --framework=<option> --org=<value> --out-dir=<value> --server-command=<value>',
+    // eslint-disable-next-line max-len
     '<%= config.bin %> <%= command.id %> --config <path/to/launch/config/file> --type <options: GitHub|FileUpload> --name=<value> --environment=<value> --branch=<value> --build-command=<value> --framework=<option> --org=<value> --out-dir=<value> --variable-type="Import variables from a stack" --alias=<value>',
+    // eslint-disable-next-line max-len
     '<%= config.bin %> <%= command.id %> --config <path/to/launch/config/file> --type <options: GitHub|FileUpload> --name=<value> --environment=<value> --branch=<value> --build-command=<value> --framework=<option> --org=<value> --out-dir=<value> --variable-type="Manually add custom variables to the list" --env-variables="APP_ENV:prod, TEST_ENV:testVal"',
   ];
 
@@ -62,6 +66,7 @@ export default class Launch extends BaseCommand<typeof Launch> {
     'variable-type': Flags.string({
       options: [...config.variablePreparationTypeOptions],
       description:
+        // eslint-disable-next-line max-len
         '[optional] Provide a variable type. <options: Import variables from a stack|Manually add custom variables to the list|Import variables from the .env.local file|Skip adding environment variables>',
     }),
     'show-variables': Flags.boolean({
@@ -79,6 +84,7 @@ export default class Launch extends BaseCommand<typeof Launch> {
     }),
     'env-variables': Flags.string({
       description:
+        // eslint-disable-next-line max-len
         '[optional] Provide the environment variables in the key:value format, separated by comma. For example: APP_ENV:prod, TEST_ENV:testVal.',
     }),
     'redeploy-latest': Flags.boolean({
@@ -103,7 +109,7 @@ export default class Launch extends BaseCommand<typeof Launch> {
     // NOTE pre-check: manage flow and set the provider value
     await this.preCheckAndInitConfig();
     if (!this.sharedConfig.isExistingProject) {
-      await this.selectProjectType();
+      await this.setProviderType();
     }
     await this.manageFlowBasedOnProvider();
   }
@@ -181,5 +187,15 @@ export default class Launch extends BaseCommand<typeof Launch> {
     });
 
     this.sharedConfig.provider = selectedProvider;
+  }
+
+  private async setProviderType(): Promise<void> {
+    const providerTypeFlag = this.flags.type;
+
+    if (providerTypeFlag) {
+      this.sharedConfig.provider = providerTypeFlag as Providers;
+      return;
+    }
+    await this.selectProjectType();
   }
 }
